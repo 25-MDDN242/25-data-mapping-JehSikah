9b7e64f56246fdcdee28119017dc43fea0259f40
JehSikah <jess.tia.merriman@gmail.com>
2025-05-19 23:48:33 +1200

    layerings and missing textures

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
+let layerCount = 0;
+
 // change these three lines as appropiate
 let sourceFile = "input_1.jpg";
 let maskFile   = "mask_1.png";
@@ -11,78 +13,74 @@ function preload() {
   sourceImg = loadImage(sourceFile);
   maskImg = loadImage(maskFile);
   //console.log(p5.Renderer2D);
-
 }
-function setup () {
+
+function setup() {
   let main_canvas = createCanvas(1920, 1080);
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
   angleMode(DEGREES);
   noStroke();
-  //background(100, 0, 200);
   background(0, 0, 0);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
-function draw () {
+function draw() {
+  //draw missing texture background
+  if (layerCount == 0) {
+    let fade = map(renderCounter, 0, 100, 255, 0);
 
-  if (renderCounter == 0) {
-    //missing();
-  }
+    missingTexture();
+    fill(0, 0, 0, fade);
+    rect(0, 0, width, height);
 
-  for(let i=0;i<4000;i++) {
+    renderCounter++;
+  } else if (layerCount == 1) {
+    for(let i=0;i<10;i++) {
+      randPixel(0);
+    }
+
+    renderCounter++;
+  } else if (layerCount == 2) {
+    for(let i=0;i<10;i++) {
+        randPixel();
+    	}
+
+    renderCounter++;
+  } else if (layerCount == 3) {
     
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pixData = sourceImg.get(x, y);
-    let maskData = maskImg.get(x, y);
 
-    let aStart = floor(random(360));
-    let aStop = floor(random(360));
-
-    let line1 = floor(random(20));
-    let line2 = floor(random(20));
-
-    fill(pixData);
-    if(maskData[0] < 128) {
-      let pointSize = 20;
-      //ellipse(x, y, pointSize, pointSize);
-      arc(x, y, pointSize, pointSize, aStart, aStop);
-      // push();
-      // rectMode(CENTER);
-      // strokeWeight(0.5);
-      // stroke(0);
-      // rect(x, y, pointSize);
-      // pop();
-    }
-    else {
-      // let pointSize = 3;
-      // ellipse(x, y, pointSize, pointSize);
-      push();
-      stroke(pixData);
-      line(x + line1, y + line1, x - line2, y - line2);
-      pop();
-    }
-  }
-  renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+    renderCounter++;
+  } else if (layerCount == 20) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
     // saveArtworkImage(outputFile);
   }
+
+
+  if (layerCount == 0 && renderCounter > 100) {
+    layerCount = 1;
+    renderCounter = 0;
+  } else if (layerCount == 1 && renderCounter > 1500) {
+    layerCount = 2;
+    renderCounter = 0;
+  } else if (layerCount == 2 && renderCounter > 1500) {
+    layerCount = 20;
+    renderCounter = 0;
+  }
+
+  console.log("Layer: " + layerCount + "  Render: " + renderCounter);
+  
 }
 
-function missing() {
-  push();
-  noStroke();
-
-  fill(255, 0, 220);
-  let boxSize = 30;
+function missingTexture() {
+  let boxSize = 20;
   let gap = boxSize*2;
 
+  fill(255, 0, 220); //missing texture purp
   for(let i = 0; i <= width; i+=gap){
     for(let j = 0; j <= height; j+=gap)
     rect(i, j, boxSize, boxSize);
@@ -92,8 +90,29 @@ function missing() {
     for(let j = 0; j <= height; j+=gap)
     rect(i + boxSize, j + boxSize, boxSize, boxSize);
   }
-  
-  pop();
+}
+
+function randPixel(filler) {
+  let x = floor(random(sourceImg.width));
+  let y = floor(random(sourceImg.height));
+  let pixData = sourceImg.get(x, y);
+  let maskData = maskImg.get(x, y);
+
+  if (filler === undefined) {
+    fill(pixData);
+  } else {
+    fill(filler)
+  }
+
+  if(maskData[0] < 128) {
+    let pointSize = 20;
+    push();
+    rectMode(CENTER);
+    // strokeWeight(0.5);
+    // stroke(0);
+    rect(x, y, pointSize);
+    pop();
+  }
 }
 
 function keyTyped() {
